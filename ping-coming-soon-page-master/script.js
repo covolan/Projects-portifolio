1c2b8d02d830582298e75a6c43e52388765ce67e
covolan <covolan09@gmail.com>
2023-09-28 16:26:31 -0300

    Finished the ping coming soon page

--- ping-coming-soon-page-master/script.js
+++ ping-coming-soon-page-master/script.js
@@ -6,7 +6,7 @@ const myReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61
 form.addEventListener('submit', (event) => {
     if (!myReg.test(emailInput.value)) {
         event.preventDefault();
-        emailInput.style.border = "1px solid red";
+        emailInput.style.border = "1px solid hsl(354, 100%, 66%)";
         errorMsg.style.display = "block";
     }
 })
