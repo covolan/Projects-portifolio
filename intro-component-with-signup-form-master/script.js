96495643ad381d541b2c4cf3d49e042926e68200
covolan <covolan09@gmail.com>
2023-09-30 16:48:30 -0300

    Finished the challenge

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -7,30 +7,20 @@ const errorMessage = document.querySelectorAll(".error");
 const errorIcon = document.querySelectorAll(".error-icon");
 
 form.addEventListener("submit", (event) => {
-    for(let i = 0; i < input.length; i++) {
-        inputHandler(event, input[i], errorMessage[i], errorIcon[i])
-    }
+  for (let i = 0; i < input.length; i++) {
+    inputHandler(event, input[i], errorMessage[i], errorIcon[i]);
+  }
 });
 
 function inputHandler(event, input, msgError, errorIcon) {
-  if (input.id == "email") {
-    if (
-      input.value == "" ||
-      !emailRegex.test(input.value) ||
-      input.value == null
-    ) {
-      event.preventDefault();
-      msgError.classList.remove("hidden-element");
-      errorIcon.classList.remove("hidden-element");
-      input.classList.add("red-border");
-    } else {
-      msgError.classList.add("hidden-element");
-      errorIcon.classList.add("hidden-element");
-      input.classList.remove("red-border");
-    }
-  }
-
-  if (input.value == "" || input.value == null) {
+  if (
+    input.value == "" ||
+    input.value == null ||
+    (input.id == "email" &&
+      (input.value == "" ||
+        !emailRegex.test(input.value) ||
+        input.value == null))
+  ) {
     event.preventDefault();
     msgError.classList.remove("hidden-element");
     errorIcon.classList.remove("hidden-element");
