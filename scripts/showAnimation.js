9c92d44aafa2f6c148da6a69d75c0d1037ff0f90
covolan <covolan09@gmail.com>
2023-10-03 18:29:34 -0300

    Updated the main page

---
+++ scripts/showAnimation.js
@@ -0,0 +1,13 @@
+const elems = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    })
+})
+
+elems.forEach((elem) => {
+    observer.observe(elem);
+})
